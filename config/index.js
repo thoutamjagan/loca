6ac6d24cb0189e987a3e8582530bd171667fc945
Jagadish Thoutam <35616820+thoutamjagan@users.noreply.github.com>
2019-03-21 23:19:16 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -4,9 +4,9 @@ const fs = require('fs');
 const configdir = process.env.LOCA_CONFIG_DIR || process.env.CONFIG_DIR || path.join(__dirname, '..', 'config');
 const website = JSON.parse(fs.readFileSync(path.join(configdir, 'website.json'), 'utf8'));
 
-let demomode = process.env.LOCA_DEMOMODE || process.env.DEMO_MODE || true;
+let demomode = process.env.LOCA_DEMOMODE || process.env.DEMO_MODE || false;
 if (demomode && typeof(demomode) !== 'boolean') {
-    demomode = demomode.toLowerCase() === 'true';
+    demomode = demomode.toLowerCase() === 'false';
 }
 
 let subscription = process.env.LOCA_PRODUCTIVE || process.env.PRODUCTIVE || false;
